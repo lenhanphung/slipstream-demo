2f09819d7c4865e1c21c6539ed6528b38252aa3a
Le Nhan Phung <lenhanphung@gmail.com>
2025-11-19 00:35:49 +0700

    Setup environment and install dependencies: Vue.js 3 and Vite plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+// Vue app initialization will be added when components are created
+// For now, this file is ready for Vue 3 setup
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Vue app will be mounted here when components are ready
+});

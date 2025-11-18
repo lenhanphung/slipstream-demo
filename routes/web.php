d8699b9e3dd30c1c887a5c393840d25a2ec7d66e
Le Nhan Phung <lenhanphung@gmail.com>
2025-11-19 00:51:05 +0700

    Set customers page as home page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/customers', function () {
     return view('customers.index');
 });
